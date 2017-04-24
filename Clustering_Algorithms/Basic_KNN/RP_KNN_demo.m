1abc7a78606f4f7864c0ce52e81cf60f95debfbc
Keegan <giantofflame@gmail.com>
2017-04-24 01:54:55 -0400

    update clustering algorithm

--- Clustering_Algorithms/Basic_KNN/RP_KNN_demo.m
+++ Clustering_Algorithms/Basic_KNN/RP_KNN_demo.m
@@ -1,11 +1,16 @@
 function [ ] = RP_KNN_demo(data_name, nsims)
 
   % Demo KNN with Random Projections
-  % Class homework for BTRY 6520
+  % Inspired by class homework for BTRY 6520
+  
+  % Type of matrix used (change this if needed)
+  option = 'binary';
+  opt_para = 0;
 
   if strcmp(data_name, 'mnist')
     [XTrain, XTest, YTrain, YTest] = load_MNIST();
     kvec = [20, 50, 100, 150, 200, 250];
+    true_par = 0.9688;
     % Baseline error for MNIST is.....
     % 0.9688 for p = 2
   elseif strcmp(data_name, 'gisette')
@@ -14,6 +19,7 @@ function [ ] = RP_KNN_demo(data_name, nsims)
     XTest = csvread('gisette_valid.csv');
     YTest = csvread('gisette_valid_label.csv');
     kvec = [10, 20, 30, 40, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500];
+    true_par = 0.9560;
     % 0.9560 for p = 2
   elseif strcmp(data_name, 'arcene')
     XTrain = csvread('arcene_train.csv');
@@ -21,6 +27,7 @@ function [ ] = RP_KNN_demo(data_name, nsims)
     XTest = csvread('arcene_valid.csv');
     YTest = csvread('arcene_valid_labels.csv');
     kvec = [10, 20, 30, 40, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000];
+    true_par = 0.7800;
     %  0.7800 p = 2
   
   end
@@ -32,27 +39,60 @@ function [ ] = RP_KNN_demo(data_name, nsims)
   max_k = max(kvec);
   err_mat = zeros(length(kvec), nsims);
 
-
+  figure;
   for iter = 1:nsims
     iter
-    big_V = gen_typeof_V([XTrain; XTest], max_k, 'binary', 1);
+    big_V = gen_typeof_V([XTrain; XTest], max_k, 'option', option, 'opt_para', opt_para);
     VXTrain = big_V(1:size_Train,:);
     VXTest = big_V((size_Train+1):end,:);
 
     for kval = 1 : length(kvec)
       %kval
       k = kvec(kval);
-      labels = basic_KNN( VXTrain(:,1:k)/sqrt(k), VXTest(:,1:k)/sqrt(k), YTrain, 2, near_neigh, 100);
+      labels = basic_KNN( VXTrain(:,1:k), VXTest(:,1:k), YTrain, 2, near_neigh, 100);
       err_mat(kval, iter) = sum(labels == YTest)/size_Test;
     end
+
+    if mod(iter, 20) == 0
+      [ avg_err, sd_err] = compute_acc_err_KNN(err_mat(:,1:iter), data_name, iter, false);
+      clf('reset')    
+      local_plot_rel_err(kvec, true_par, avg_err,sd_err, iter, data_name, false)
+      drawnow
+    end
+
     if mod(iter, 100) == 0
-      compute_acc_err_KNN(err_mat(:,1:iter), data_name, iter)
+      compute_acc_err_KNN(err_mat(:,1:iter), data_name, iter, true);
     end
   end
 
-  compute_acc_err_KNN(err_mat(:,1:iter), data_name, iter)
-
+  [ avg_err, sd_err] = compute_acc_err_KNN(err_mat(:,1:iter), data_name, iter, 'tosave', true);
+  
+  
+  local_plot_rel_err(kvec, true_par, avg_err,sd_err,iter, data_name, true)
 
 end
 
 
+function [ ] = local_plot_rel_err(kvec, true_par, avg_err, sd_err, iter, data_name, fig_para)
+
+  if fig_para
+    figure;
+  end
+
+  plot(kvec,avg_err, '-r', 'DisplayName', 'RP Misclassification Error'); hold all
+  plot(kvec,repmat(true_par,1,length(kvec)), '-b', 'DisplayName', 'Misclassification Error');
+  plot(kvec, avg_err + sd_err, '--m',  'DisplayName', '1SD errs')
+  plot(kvec, avg_err - sd_err, '--m', 'DisplayName', '1SD errs')
+  plot(kvec, avg_err + 2*sd_err, '--c', 'DisplayName', '2SD errs')
+  plot(kvec, avg_err - 2*sd_err, '--c', 'DisplayName', '2SD errs')
+  plot(kvec, avg_err + 3*sd_err, '--g', 'DisplayName', '3SD errs')
+  plot(kvec, avg_err - 3*sd_err, '--g', 'DisplayName', '3SD errs')
+
+
+  grid on;
+  title(['Misclassification error for ', data_name, ' at ', num2str(iter), ' iterations'], 'FontWeight', 'bold');
+  xlabel('Number of columns K', 'FontWeight', 'bold');
+  legend('-DynamicLegend', 'location', 'southeast');
+  ylabel('Missclassificaton Error', 'FontWeight', 'bold');
+
+end
--- Clustering_Algorithms/Basic_KNN/compute_acc_err_KNN.m
+++ Clustering_Algorithms/Basic_KNN/compute_acc_err_KNN.m
@@ -1,17 +1,20 @@
-function [ ] = compute_acc_err_KNN(err_mat, data_name, nsims)
+function [ avg_err, sd_err] = compute_acc_err_KNN(err_mat, data_name, nsims, tosave)
 
   % Simply computes mean classisfication error and its standard deviation
-  % from a matrix storing classification erros at varying iterations and projection col number
+  % from a matrix storing classification errors at varying iterations and projection col number
+
+
 
   avg_err = mean(err_mat,2);
   sd_err = sqrt(var(err_mat,0,2));
   
-  str_var = ['knn_data/', data_name, '_avg_err_with_', num2str(nsims), '_iter.csv'];
-  csvwrite(str_var, avg_err);  
+  if tosave
+    str_var = ['knn_data/', data_name, '_avg_err_with_', num2str(nsims), '_iter.csv'];
+    csvwrite(str_var, avg_err);  
   
-  str_var = ['knn_data/', data_name, '_sd_with_', num2str(nsims), '_iter.csv'];
-  csvwrite(str_var, sd_err);  
-  
+    str_var = ['knn_data/', data_name, '_sd_with_', num2str(nsims), '_iter.csv'];
+    csvwrite(str_var, sd_err);  
+  end
 
 end
 
